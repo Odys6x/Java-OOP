c321ad65aabd8fc2e3a2c5c006a8edaba7c95279
mcspicyupsiz <nigelong2124@gmail.com>
2024-02-19 18:27:44 +0800

    updated SceneManager and gameoverScreen. just testing

---
+++ games/desktop/src/com/mygdx/game/SceneMgmt/SceneManager.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.SceneMgmt;
+import java.util.ArrayList;
+
+public class SceneManager {
+    private static SceneManager instance;
+    public static final int Menu = 0;
+    public static final int Game = 1;
+    public static final int End = 2;
+    public static final int Gameover = 3;
+
+    private ArrayList<sceneScreen> scenes; // add the scenes to an array
+    private int currentScene; // this is the current active screen
+
+    public static SceneManager getInstance() { // to ensure that only one instance of scenemanager exists
+        if(instance == null){
+            instance = new SceneManager();
+        }
+        return instance;
+    }
+    private SceneManager(){ // init all the screens
+        scenes = new ArrayList<>();
+        scenes.add(new menuScreen());
+        scenes.add(new gameScreen());
+        scenes.add(new endScreen());
+        scenes.add(new gameoverScreen());
+        currentScene = Menu;
+    }
+}   
+
+abstract class sceneScreen{
+
+}
