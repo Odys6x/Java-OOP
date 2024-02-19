5fd6b7e99b875c66ceb15f1b8087e11a56be4e86
mcspicyupsiz <nigelong2124@gmail.com>
2024-02-20 00:09:14 +0800

    Added Background.png, changed render to super.render, added scenes to create and dispose as well

--- games/desktop/src/com/mygdx/game/GameMaster.java
+++ games/desktop/src/com/mygdx/game/GameMaster.java
@@ -1,20 +1,28 @@
 package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.mygdx.game.SceneMgmt.SceneManager;
+import com.mygdx.game.SceneMgmt.menuScreen;
 import com.mygdx.game.SimulationMgmt.Simulation;
 
-public class GameMaster extends ApplicationAdapter {
+public class GameMaster extends Game { //changed from application adapter to game
     private Simulation Simulation;
+    private SceneManager SceneManager;
     @Override
     public void create() {
         Simulation = new Simulation();
         Simulation.initialise();
+        this.setScreen(new menuScreen(SceneManager));
     }
     @Override
     public void render() {
-        Simulation.update();
+        super.render();
+        // Simulation.update();
+        // SceneManager.render();
     }
     @Override
     public void dispose() {
         Simulation.end();
+        SceneManager.dispose();
     }
 }
