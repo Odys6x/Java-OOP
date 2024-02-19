8b7180096200b30076e4fbc5893ccb1bc458e6c4
mcspicyupsiz <nigelong2124@gmail.com>
2024-02-20 02:16:26 +0800

    initially the previous version didnt display the background.png, managed to fix it

--- games/desktop/src/com/mygdx/game/SceneMgmt/menuScreen.java
+++ games/desktop/src/com/mygdx/game/SceneMgmt/menuScreen.java
@@ -1,66 +1,62 @@
 package com.mygdx.game.SceneMgmt;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.utils.Align;
 
-public class menuScreen extends SceneScreen {
-    private Texture backgroundTexture;
-    private BitmapFont titleFont, optionFont;
-    private String[] options = {"Start", "Quit"};
-    private int currentOption = 0;
-    private SpriteBatch batch;
-    
-    public menuScreen(){
-        // backgroundTexture = new Texture(""); can add in the future when needed. (can be gif or image)
-        titleFont = new BitmapFont();
-        optionFont = new BitmapFont();
-        batch = new SpriteBatch();
+import com.badlogic.gdx.Gdx; 
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20; 
+import com.badlogic.gdx.graphics.Texture; 
+import com.badlogic.gdx.scenes.scene2d.InputEvent; 
+import com.badlogic.gdx.scenes.scene2d.Stage; 
+import com.badlogic.gdx.scenes.scene2d.ui.Image; 
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener; 
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-        // Setup fonts (we need to go and add the font files)
-        titleFont.getData().setScale(2);
-        optionFont.getData().setScale(1.5f);
+public class menuScreen extends SceneScreen{
+    private SceneManager SceneManager;
+    private Texture background;
+    private Stage stage;
+    public menuScreen (SceneManager SceneManager){
+        super();
+        this.SceneManager = SceneManager.getInstance();
     }
+    
     @Override
     public void show() {
-    }
+        stage = new Stage(new ScreenViewport());
+        background = new Texture(Gdx.files.internal("background.png"));
 
+        Image backgroundImage = new Image(background);
+        backgroundImage.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        stage.addActor(backgroundImage); // adds the background image to stage
+
+        Gdx.input.setInputProcessor(stage); // sets the input processor to handle input events for actors
+
+        stage.addListener(new ClickListener(){ // adds a listener to the stage for touch events
+
+            // when clicked will transition to the next scene
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                Gdx.app.log("menuScreen", "Screen clicked, attempting to transition to gameScreen.");
+                System.out.println("Next Screen (Menu Screen)");
+                if (SceneManager != null) {
+                    SceneManager.setScene(new gameScreen(SceneManager)); // supposed to transition here. but not working
+                } else {
+                    System.out.println("SceneManager is null"); // error handling cos game screen not transitioning..
+                }
+                            }
+        });
+    }
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-
-        // draw the background
-        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        // draw title and options
-        titleFont.draw(batch, "Game Title", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() - 100, 0, Align.center, false);
-        for (int i = 0; i < options.length; i++) {
-            if (i == currentOption) {
-                optionFont.setColor(Color.RED);
-            } else {
-                optionFont.setColor(Color.WHITE);
-            }
-            optionFont.draw(batch, options[i], Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() - 200 - 50 * i, 0, Align.center, false);
-        }
-
-        batch.end();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clear screen
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw(); // draws the stage and the actors
     }
 
     @Override
-    public void dispose() {
-        // Dispose resources
-        backgroundTexture.dispose();
-        titleFont.dispose();
-        optionFont.dispose();
-        batch.dispose();
+    public void dispose(){
+        background.dispose();
     }
 
-    // will add input handling ltr
 }
-    
