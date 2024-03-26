e7d4d49e42bc54b1b77e6da3b3b1fb1393147bbb
mcspicyupsiz <nigelong2124@gmail.com>
2024-03-27 01:02:14 +0800

    removed sound from menu screen

--- games/desktop/src/com/mygdx/game/SceneMgmt/MenuScreen.java
+++ games/desktop/src/com/mygdx/game/SceneMgmt/MenuScreen.java
@@ -10,21 +10,16 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image; 
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener; 
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.mygdx.game.SoundMgmt.Sound;
-
 
 public class MenuScreen extends SceneScreen{
     private SceneManager sceneManager;
     private Texture background;
     private Stage stage;
     private Skin skin;
-    private Sound sound = new Sound("games/assets/music.wav");
 
     public MenuScreen (SceneManager sceneManager){
         super();
         this.sceneManager = sceneManager;
-        sound.play();
-        sound.setVolume(0.2f);
     }
     
     @Override
@@ -111,7 +106,6 @@ public class MenuScreen extends SceneScreen{
         background.dispose();
         skin.dispose();
         stage.dispose();
-        sound.close();
     }
 
 }
