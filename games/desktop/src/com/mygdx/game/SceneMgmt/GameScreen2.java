2afb3cee011ebab2c9bd543dad4efbc25aa598c3
mcspicyupsiz <nigelong2124@gmail.com>
2024-03-28 17:52:07 +0800

    sound fix

--- games/desktop/src/com/mygdx/game/SceneMgmt/GameScreen2.java
+++ games/desktop/src/com/mygdx/game/SceneMgmt/GameScreen2.java
@@ -21,7 +21,7 @@ public class GameScreen2 extends SceneScreen {
     private Simulation simulation; 
     private Texture background;
     private Stage stage;
-    private Sound sound = new Sound("game/assets/music.wav");
+    private Sound sound = new Sound("games/assets/music.wav");
     private BitmapFont font;
     private ScoreManager scoreManager;
     private SpriteBatch spriteBatch;
